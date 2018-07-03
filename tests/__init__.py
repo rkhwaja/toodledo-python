e49b70db1ac4dca90e92e0f50f6a60db7d2d31ab
Rehan Khwaja <rehan@khwaja.name>
2018-07-03 07:10:41 -0700

    Switch to pytest

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+# Make the tests a module so that pytest will pick up the source code of the toodledo module for testing
---
+++ tests/test_read.py
@@ -0,0 +1,15 @@
+from os import environ
+
+from pytest import fixture
+
+@fixture
+def toodledo():
+	from toodledo import TokenStorageFile, Toodledo
+	tokenStorage = TokenStorageFile(environ["TOODLEDO_TOKEN_STORAGE"])
+	return Toodledo(clientId=environ["TOODLEDO_CLIENT_ID"], clientSecret=environ["TOODLEDO_CLIENT_SECRET"], tokenStorage=tokenStorage, scope="basic tasks notes folders write")
+
+def test_get_account(toodledo):
+	account = toodledo.GetAccount()
+
+def test_get_tasks(toodledo):
+	tasks = toodledo.GetTasks(params={})
