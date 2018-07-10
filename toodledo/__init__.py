805cfb1dd66ea7cc6fb69a911c844ae6796ba3d8
Rehan Khwaja <rehan@khwaja.name>
2018-07-10 12:51:21 -0700

    Remove non-public classes and methods from package exports

--- toodledo/__init__.py
+++ toodledo/__init__.py
@@ -1,1 +1,3 @@
-from .toodledo import Account, AddTasks, DeleteTasks, EditTasks, GetAccount, GetTasks, Task, TokenStorageFile, Toodledo, ToodledoDate, ToodledoDatetime, ToodledoError, ToodledoTags
+"""Python wrapper for the Toodledo v3 API which is documented at http://api.toodledo.com/3/"""
+
+from .toodledo import AddTasks, DeleteTasks, EditTasks, GetAccount, GetTasks, Task, TokenStorageFile, Toodledo, ToodledoError
