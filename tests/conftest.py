2ba2440d0738ef3e5b1648fc1cdf3c018dbc1cb4
Rehan Khwaja <rehan@khwaja.name>
2019-08-23 23:13:46 -0400

    Can't read environment values through API

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,22 +17,24 @@ class TokenReadOnly:
 	"""Read the API tokens from an environment variable"""
 
 	def __init__(self):
-		self.token = self.Load()
+		self.token = loads(environ["TOODLEDO_TOKEN_READONLY"])
 
 	def Save(self, token): # pylint: disable=no-self-use
 		"""Do nothing - this may cause a problem if the refresh token changes"""
 		travis_env.update(environ["TRAVIS_REPO"], TOODLEDO_TOKEN_READONLY=EscapeForBash(token))
+		self.token = token
 
 	def Load(self): # pylint: disable=no-self-use
 		"""Load and return the token. Called by Toodledo class"""
-		travisRepo = environ["TRAVIS_REPO"]
-		info(f"travisRepo: {travisRepo}")
-		allVars = travis_env.vars(travisRepo)
-		info(f"allVars: {allVars}")
-		test_var = allVars["test_var"]
-		info(f"test_var: {test_var}")
-		assert loads(test_var)["A"] == "&"
-		return loads(allVars["TOODLEDO_TOKEN_READONLY"])
+		# travisRepo = environ["TRAVIS_REPO"]
+		# info(f"travisRepo: {travisRepo}")
+		# allVars = travis_env.vars(travisRepo)
+		# info(f"allVars: {allVars}")
+		# test_var = allVars["test_var"]
+		# info(f"test_var: {test_var}")
+		# assert loads(test_var)["A"] == "&"
+		# return loads(allVars["TOODLEDO_TOKEN_READONLY"])
+		return self.token
 
 @fixture
 def toodledo():
